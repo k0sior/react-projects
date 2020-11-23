c27aaaeb3b7e65873f74067274c5c854866568e5
k0sior <mac.kosiorek@gmail.com>
2020-11-23 15:49:51 +0100

    project

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, ipsa nulla inventore rem alias quod provident necessitatibus natus asperiores illo earum, iste nisi, ducimus error sint esse cupiditate soluta tempore.</p>
+    </section>
   )
 }
 
--- 15-cocktails/setup/src/pages/Error.js
+++ 15-cocktails/setup/src/pages/Error.js
@@ -3,9 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
+    <section className="section error-page">
+      <div className="error-container">
+      <h1>error #404 - NO PAGE FOUND</h1>
+      <Link to="/" className="btn btn-primary">
+        Back Home
+      </Link>
+
+      </div>
+    </section>
   )
 }
 
